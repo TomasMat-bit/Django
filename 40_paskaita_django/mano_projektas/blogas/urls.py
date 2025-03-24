94e96aee33f935606e8fa9be1f021a58d77a0c48
Tomas Matukas <tomas.matukas@gmail.com>
2025-03-24 21:42:16 +0200

    40day, djago task, 3part of task, apie changed

---
+++ 40_paskaita_django/mano_projektas/blogas/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from setuptools.extern import names
+
+from . import views
+
+urlpatterns = [
+    path('', views.pagrindinis, name='pagrindinis'),
+    path('apie/', views.apie, name='apie'),
+    path('kontaktai/', views.kontaktai, name='kontaktai'),
+    path('naujienos/', views.naujienos, name='naujienos'),
+]
+
--- 40_paskaita_django/mano_projektas/blogas/views.py
+++ 40_paskaita_django/mano_projektas/blogas/views.py
@@ -9,4 +9,20 @@ def pagrindinis(request):
     return render(request, 'pagrindinis.html', kontekstas)
 
 def apie(request):
-    return HttpResponse('Cia yra apie puslapis')
+    kontekstas = {
+        'pavadinimas': 'Apie mus',
+        'antraste': 'Informacija apie mus'
+    }
+    return render(request, 'apie.html', kontekstas)
+
+def kontaktai(request):
+    return HttpResponse('Susisiekite su mumis!')
+
+def naujienos(request):
+    kontekstas = {
+        'pavadinimas': 'Naujienos',
+        'antraste': 'Šviežiausios naujienos'
+    }
+    return render(request, 'naujienos.html', kontekstas)
+
+
